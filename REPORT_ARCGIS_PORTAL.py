a1bdaa200c67286b8ed0c6aebc1c6c55bc26ce8e
andapra <alexanderdan.pratama@gmail.com>
2022-04-04 20:10:19 +0700

    creating oop

--- REPORT_ARCGIS_PORTAL.py
+++ REPORT_ARCGIS_PORTAL.py
@@ -4,94 +4,103 @@ from arcgis.mapping import WebMap
 import os
 import pandas as pd
 
-def main(url, username, password, reportingPath):
-    gis = GIS(url, username, password)
-    reportingPath = os.getcwd() # change this path if you want to save other address
+class reporting_portal():
+    def __init__(self, portal, username, password, reportingPath) -> None:
+        self.portal = portal
+        self.username = username
+        self.password = password
+        self.reportingPath = reportingPath
 
-    reportingFile = 'reporting'
-    itemTypeGIS = ['Feature Layer', 'Geoprocessing Toolbox', 'Map Image Layer', 'Data Store', 'Web Map',
-            'Web Experience', 'Dashboard']
+    def main(self):
+        gis = GIS(self.url, self.username, self.password)
+        reportingPath = self.reportingPath # change this path if you want to save other address
 
-    listRAW = []
-    listService = []
-    listUsers = gis.users.search('')
-    for user in listUsers:
-        getUser = user['username']
+        reportingFile = 'reporting'
+        itemTypeGIS = ['Feature Layer', 'Geoprocessing Toolbox', 'Map Image Layer', 'Data Store', 'Web Map',
+                'Web Experience', 'Dashboard']
 
-        for item in itemTypeGIS:
-            getItemType = item
-            contentGIS = gis.content.search(query="owner:" + getUser, item_type=getItemType, max_items=1000)
+        listRAW = []
+        listService = []
+        listUsers = gis.users.search('')
+        for user in listUsers:
+            getUser = user['username']
 
-            if len(contentGIS) == 0:
-                pass
-            else:
-                if getItemType == 'Web Map' or getItemType == 'Dashboard':
-                    for i in range(len(contentGIS)):
-                        getData = contentGIS[i]['title']
-                        getItem = contentGIS[i].id
+            for item in itemTypeGIS:
+                getItemType = item
+                contentGIS = gis.content.search(query="owner:" + getUser, item_type=getItemType, max_items=1000)
 
-                        listLayerURL = []
-                        try:
-                            getLayers = WebMap(contentGIS[i]).layers
-                            for layer in getLayers:
-                                layerURL = layer['url']
-                                listLayerURL.append(layerURL)
-                        except:
-                            listLayerURL = 'FAILED TO GET URL LIST'
-
-                        itemStatus = Item(gis, contentGIS[i].id)
-                        shareStatus = itemStatus.shared_with
-                        shareStatus_Public = shareStatus['everyone']
-                        shareStatus_Org = shareStatus['org']
-                        shareStatus_Group = shareStatus['groups']
-
-                        rowDict = {'title': getData, 'type': getItemType, 'owner': getUser,
-                                   'url': contentGIS[i].homepage, 'itemid': contentGIS[i].id,
-                                   'shared_public': shareStatus_Public,
-                                   'shared_org': shareStatus_Org,
-                                   'shared_groups': str(shareStatus_Group),
-                                   'layers': '{}'.format(str(listLayerURL))}
-                        listService.append(rowDict)
-                        listRAW.append(contentGIS[i])
-
+                if len(contentGIS) == 0:
+                    pass
                 else:
-                    for i in range(len(contentGIS)):
-                        getData = contentGIS[i]['title']
-                        getItem = contentGIS[i].id
+                    if getItemType == 'Web Map' or getItemType == 'Dashboard':
+                        for i in range(len(contentGIS)):
+                            getData = contentGIS[i]['title']
+                            getItem = contentGIS[i].id
 
-                        itemStatus = Item(gis, contentGIS[i].id)
-                        shareStatus = itemStatus.shared_with
-                        shareStatus_Public = shareStatus['everyone']
-                        shareStatus_Org = shareStatus['org']
-                        shareStatus_Group = shareStatus['groups']
+                            listLayerURL = []
+                            try:
+                                getLayers = WebMap(contentGIS[i]).layers
+                                for layer in getLayers:
+                                    layerURL = layer['url']
+                                    listLayerURL.append(layerURL)
+                            except:
+                                listLayerURL = 'FAILED TO GET URL LIST'
 
-                        rowDict = {'title': getData, 'type': getItemType, 'owner': getUser,
-                                   'url': contentGIS[i].url, 'itemid': contentGIS[i].id,
-                                   'shared_public': shareStatus_Public,
-                                   'shared_org': shareStatus_Org,
-                                   'shared_groups': str(shareStatus_Group),
-                                   'layers': None}
-                        listService.append(rowDict)
+                            itemStatus = Item(gis, contentGIS[i].id)
+                            shareStatus = itemStatus.shared_with
+                            shareStatus_Public = shareStatus['everyone']
+                            shareStatus_Org = shareStatus['org']
+                            shareStatus_Group = shareStatus['groups']
 
-                        listRAW.append(contentGIS[i])
+                            rowDict = {'title': getData, 'type': getItemType, 'owner': getUser,
+                                    'url': contentGIS[i].homepage, 'itemid': contentGIS[i].id,
+                                    'shared_public': shareStatus_Public,
+                                    'shared_org': shareStatus_Org,
+                                    'shared_groups': str(shareStatus_Group),
+                                    'layers': '{}'.format(str(listLayerURL))}
+                            listService.append(rowDict)
+                            listRAW.append(contentGIS[i])
 
-    df = pd.DataFrame(columns = ['title', 'type', 'owner', 'url', 'itemid', 'shared_public',
-                                 'shared_org', 'shared_groups','layers'])
-    dfAppend = df.append(listService, ignore_index=True)
-    dfAppend.to_csv(os.path.join(reportingPath, reportingFile+ '.csv'))
+                    else:
+                        for i in range(len(contentGIS)):
+                            getData = contentGIS[i]['title']
+                            getItem = contentGIS[i].id
+
+                            itemStatus = Item(gis, contentGIS[i].id)
+                            shareStatus = itemStatus.shared_with
+                            shareStatus_Public = shareStatus['everyone']
+                            shareStatus_Org = shareStatus['org']
+                            shareStatus_Group = shareStatus['groups']
+
+                            rowDict = {'title': getData, 'type': getItemType, 'owner': getUser,
+                                    'url': contentGIS[i].url, 'itemid': contentGIS[i].id,
+                                    'shared_public': shareStatus_Public,
+                                    'shared_org': shareStatus_Org,
+                                    'shared_groups': str(shareStatus_Group),
+                                    'layers': None}
+                            listService.append(rowDict)
+
+                            listRAW.append(contentGIS[i])
+
+        df = pd.DataFrame(columns = ['title', 'type', 'owner', 'url', 'itemid', 'shared_public',
+                                    'shared_org', 'shared_groups','layers'])
+        dfAppend = df.append(listService, ignore_index=True)
+        dfAppend.to_csv(os.path.join(reportingPath, reportingFile+ '.csv'))
 
 
 if __name__ == '__main__':
     startTime = datetime.today()
-    url = '' #change this url
-    username = '' #change this username
-    password = '' #change this password
 
-    reportingPath = os.getcwd()
+    portal = '' ## input portal url example: https://gis.portal.com/webadaptor
+    username = '' ## username portal that can access username
+    password = '' ## password of the username
+    export_path = os.getcwd() # change this path if you want to save other address
+    
+    information = reporting_portal(portal, username, password, export_path) 
 
     try:
         print('Starting to process')
-        main(url, username, password, reportingPath)
+        reporting_portal.main(information)
 
         print('End of the process')
         endTime = datetime.today()
--- REPORT_ARCGIS_SERVICE.py
+++ REPORT_ARCGIS_SERVICE.py
@@ -6,130 +6,137 @@ import os
 import pandas as pd
 import json
 
+class reporting_server():
+    def __init__(self, portal, username, password, reportingPath) -> None:
+        self.portal = portal
+        self.username = username
+        self.password = password
+        self.reportingPath = reportingPath
 
+    def main(self):
+        gis = GIS(self.portal, self.username, self.password)
 
-def main(portal, username, password, reportingPath):
-    gis = GIS(portal, username, password)
+        # will connect to arcgis server index 0, usually the arcgis server base deployment, you can change the other index 
+        gis_server = gis.admin.servers.list()[0]
+        service_folder = gis_server.services.folders
 
-    # will connect to arcgis server index 0, usually the arcgis server base deployment, you can change the other index 
-    gis_server = gis.admin.servers.list()[0]
-    service_folder = gis_server.services.folders
+        df = pd.DataFrame(columns=[])
+        df_properties = pd.DataFrame(columns=[])
+        df_portalproperties = pd.DataFrame(columns=[])
+        df_extension = pd.DataFrame(columns=[])
+        list_dict = []
+        list_dict_property = []
+        list_dict_portalproperty = []
+        list_dict_extension = []
+        sheet_basename = 'base'
+        for index, folder in enumerate(service_folder):
+            print('=========== {} ==========='.format(folder))
+            get_list_service = gis_server.services.list(folder= folder)
+            length_service = len(get_list_service)
+            print(folder, length_service)
+            if length_service == 0:
+                pass
+            else:
+                for subindex, service_in_folder in enumerate(get_list_service):
+                    property_service = '{}'.format(service_in_folder.properties)
+                    load_data = json.loads(property_service)
+                    dict_frame = {}
+                    sub_dictframe_properties = {}
+                    sub_dictframe_portalproperties = {}
+                    sub_dictframe_extensions = {}
+                    if index == 0 and subindex == 0:
+                        print('creating the dataframe columns')
+                        for key in load_data.keys():
+                            if key == 'properties' or key == 'portalProperties':
+                                sheet_name = key
+                                get_subvalue = load_data['{}'.format(key)]
 
-    df = pd.DataFrame(columns=[])
-    df_properties = pd.DataFrame(columns=[])
-    df_portalproperties = pd.DataFrame(columns=[])
-    df_extension = pd.DataFrame(columns=[])
-    list_dict = []
-    list_dict_property = []
-    list_dict_portalproperty = []
-    list_dict_extension = []
-    sheet_basename = 'base'
-    for index, folder in enumerate(service_folder):
-        print('=========== {} ==========='.format(folder))
-        get_list_service = gis_server.services.list(folder= folder)
-        length_service = len(get_list_service)
-        print(folder, length_service)
-        if length_service == 0:
-            pass
-        else:
-            for subindex, service_in_folder in enumerate(get_list_service):
-                property_service = '{}'.format(service_in_folder.properties)
-                load_data = json.loads(property_service)
-                dict_frame = {}
-                sub_dictframe_properties = {}
-                sub_dictframe_portalproperties = {}
-                sub_dictframe_extensions = {}
-                if index == 0 and subindex == 0:
-                    print('creating the dataframe columns')
-                    for key in load_data.keys():
-                        if key == 'properties' or key == 'portalProperties':
-                            sheet_name = key
-                            get_subvalue = load_data['{}'.format(key)]
+                                if sheet_name == 'properties':
+                                    df_properties['serviceName'] = load_data['serviceName']
+                                    for subkey in get_subvalue.keys():
+                                        df_properties['{}'.format(subkey)] = get_subvalue['{}'.format(subkey)]
 
-                            if sheet_name == 'properties':
-                                df_properties['serviceName'] = load_data['serviceName']
-                                for subkey in get_subvalue.keys():
-                                    df_properties['{}'.format(subkey)] = get_subvalue['{}'.format(subkey)]
+                                elif sheet_name == 'portalProperties':
+                                    df_portalproperties['serviceName'] = load_data['serviceName']
+                                    for subkey in get_subvalue.keys():
+                                        df_portalproperties['{}'.format(subkey)] = get_subvalue['{}'.format(subkey)]
 
-                            elif sheet_name == 'portalProperties':
-                                df_portalproperties['serviceName'] = load_data['serviceName']
-                                for subkey in get_subvalue.keys():
-                                    df_portalproperties['{}'.format(subkey)] = get_subvalue['{}'.format(subkey)]
-
-                        elif key == 'extensions':
-                            sheet_name = key
-                            get_subvalue = load_data['{}'.format(key)]
-                            for subkey in get_subvalue[0].keys():
-                                df_extension['{}'.format(subkey)] = get_subvalue[0]['{}'.format(subkey)]
-
-                        else:
-                            get_value = load_data['{}'.format(key)]
-                            df['{}'.format(key)] = [get_value]
-                else:
-                    print('appending data to dataframe')
-                    for key in load_data.keys():
-                        if key == 'properties' or key == 'portalProperties':
-                            sheet_name = key
-                            get_subvalue = load_data['{}'.format(key)]
-                            if sheet_name == 'properties':
-                                sub_dictframe_properties['serviceName'] = load_data['serviceName']
-                                for subkey in get_subvalue.keys():
-                                    sub_dictframe_properties['{}'.format(subkey)] = get_subvalue['{}'.format(subkey)]
-                                list_dict_property.append(sub_dictframe_properties)
-
-                            elif sheet_name == 'portalProperties':
-                                sub_dictframe_portalproperties['serviceName'] = load_data['serviceName']
-                                for subkey in get_subvalue.keys():
-                                    sub_dictframe_portalproperties['{}'.format(subkey)] = get_subvalue['{}'.format(subkey)]
-                                list_dict_portalproperty.append(sub_dictframe_portalproperties)
-
-                        elif key == 'extensions':
-                            sheet_name = key
-                            get_subvalue = load_data['{}'.format(key)]
-                            if len(get_subvalue) == 0:
-                                pass
-                            else:
+                            elif key == 'extensions':
+                                sheet_name = key
+                                get_subvalue = load_data['{}'.format(key)]
                                 for subkey in get_subvalue[0].keys():
-                                    sub_dictframe_extensions['{}'.format(subkey)] = get_subvalue[0]['{}'.format(subkey)]
-                                list_dict_extension.append(sub_dictframe_extensions)
+                                    df_extension['{}'.format(subkey)] = get_subvalue[0]['{}'.format(subkey)]
 
-                        else:
-                            get_value = load_data['{}'.format(key)]
-                            dict_frame['{}'.format(key)] = get_value
+                            else:
+                                get_value = load_data['{}'.format(key)]
+                                df['{}'.format(key)] = [get_value]
+                    else:
+                        print('appending data to dataframe')
+                        for key in load_data.keys():
+                            if key == 'properties' or key == 'portalProperties':
+                                sheet_name = key
+                                get_subvalue = load_data['{}'.format(key)]
+                                if sheet_name == 'properties':
+                                    sub_dictframe_properties['serviceName'] = load_data['serviceName']
+                                    for subkey in get_subvalue.keys():
+                                        sub_dictframe_properties['{}'.format(subkey)] = get_subvalue['{}'.format(subkey)]
+                                    list_dict_property.append(sub_dictframe_properties)
 
-                    list_dict.append(dict_frame)
-                print('Complete in {} of {}'.format(subindex, length_service-1))
-        print('=========== {} ==========='.format(folder))
+                                elif sheet_name == 'portalProperties':
+                                    sub_dictframe_portalproperties['serviceName'] = load_data['serviceName']
+                                    for subkey in get_subvalue.keys():
+                                        sub_dictframe_portalproperties['{}'.format(subkey)] = get_subvalue['{}'.format(subkey)]
+                                    list_dict_portalproperty.append(sub_dictframe_portalproperties)
 
-        df_2 = pd.DataFrame(list_dict)
-        df_properties_2 = pd.DataFrame(list_dict_property)
-        df_portalproperties_2 = pd.DataFrame(list_dict_portalproperty)
-        df_extension_2 = pd.DataFrame(list_dict_extension)
+                            elif key == 'extensions':
+                                sheet_name = key
+                                get_subvalue = load_data['{}'.format(key)]
+                                if len(get_subvalue) == 0:
+                                    pass
+                                else:
+                                    for subkey in get_subvalue[0].keys():
+                                        sub_dictframe_extensions['{}'.format(subkey)] = get_subvalue[0]['{}'.format(subkey)]
+                                    list_dict_extension.append(sub_dictframe_extensions)
 
-        print('Compiling dataframe')
-        compiling_base = df.append(df_2, ignore_index=True)
-        compiling_properties = df_properties.append(df_properties_2)
-        compiling_portalproperties = df_portalproperties.append(df_portalproperties_2)
-        compiling_extension = df_extension.append(df_extension_2)
+                            else:
+                                get_value = load_data['{}'.format(key)]
+                                dict_frame['{}'.format(key)] = get_value
 
-    print('Generate Report')
-    with pd.ExcelWriter(os.path.join(os.getcwd(), 'reporting_server'+'.xlsx')) as writer:
-        compiling_base.to_excel(writer, sheet_name=sheet_basename)
-        compiling_properties.to_excel(writer, sheet_name='properties')
-        compiling_portalproperties.to_excel(writer, sheet_name='portalproperties')
-        compiling_extension.to_excel(writer, sheet_name='extension')
+                        list_dict.append(dict_frame)
+                    print('Complete in {} of {}'.format(subindex, length_service-1))
+            print('=========== {} ==========='.format(folder))
+
+            df_2 = pd.DataFrame(list_dict)
+            df_properties_2 = pd.DataFrame(list_dict_property)
+            df_portalproperties_2 = pd.DataFrame(list_dict_portalproperty)
+            df_extension_2 = pd.DataFrame(list_dict_extension)
+
+            print('Compiling dataframe')
+            compiling_base = df.append(df_2, ignore_index=True)
+            compiling_properties = df_properties.append(df_properties_2)
+            compiling_portalproperties = df_portalproperties.append(df_portalproperties_2)
+            compiling_extension = df_extension.append(df_extension_2)
+
+        print('Generate Report')
+        with pd.ExcelWriter(os.path.join(self.reportingPath, 'reporting_server'+'.xlsx')) as writer:
+            compiling_base.to_excel(writer, sheet_name=sheet_basename)
+            compiling_properties.to_excel(writer, sheet_name='properties')
+            compiling_portalproperties.to_excel(writer, sheet_name='portalproperties')
+            compiling_extension.to_excel(writer, sheet_name='extension')
 
 if __name__ == '__main__':
     startTime = datetime.today()
+
     portal = '' ## input portal url example: https://gis.portal.com/webadaptor
     username = '' ## username portal that can access username
     password = '' ## password of the username
-
-    reportingPath = os.getcwd() # change this path if you want to save other address
+    export_path = os.getcwd() # change this path if you want to save other address
+    
+    information = reporting_server(portal, username, password, export_path) 
 
     try:
         print('Starting to process')
-        main(portal, username, password, reportingPath)
+        reporting_server.main(information)
         print('End of the process')
         endTime = datetime.today()
         deltaTime = endTime - startTime
